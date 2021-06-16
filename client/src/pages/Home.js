436692b308fbfd753e88507d9decad1dfdf4910a
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-16 16:09:22 -0700

    thoughts queried correctly

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
+
+const Home = () => {
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+  return (
+    <main>
+      <div className='flex-row justify-space-between'>
+        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
