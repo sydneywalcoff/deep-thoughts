21625e2ac47c40049a41c1c44783d91ad933e682
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-06-22 11:50:59 -0700

    added handle submit

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -11,12 +11,18 @@ const ThoughtForm = () => {
         }
     }
 
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setText('');
+        setCharacterCount(0);
+    }
+
     return (
         <div>
             <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
                 Character Count: {characterCount}/280
             </p>
-            <form className="flex-row justify-center justify-space-between-md align-stretch">
+            <form className="flex-row justify-center justify-space-between-md align-stretch" onSubmit={handleSubmit}>
                 <textarea
                     placeholder="Here's a new thought..."
                     className="form-input col-12 col-md-9"
